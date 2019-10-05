12e2cec986b542976d21e960c627cd9932e390b6
DadaNanjesha <dadananjesha.rymec@gmail.com>
2019-10-05 13:41:26 +0530

    PROJECT

---
+++ QRApp/models.py
@@ -0,0 +1,16 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+from django.db import models
+
+
+# Create your models here.
+class Gtin(models.Model):
+    gtin_number = models.CharField(max_length=30)
+    product_name = models.CharField(max_length=30)
+
+    def __str__(self):
+        return self.product_name
+
+    class Meta:
+        verbose_name_plural = "GTIN NUMBER"
+        verbose_name = "GTIN NUMBER"
